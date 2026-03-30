26b79878f6892ae68b29bec429396ae46557eea6
ZisisKi <kiratsoulis95@hotmail.com>
2026-03-30 13:56:26 +0300

    Remove comments

--- backend/src/main/java/com/coffeeapp/backend/config/WebConfig.java
+++ backend/src/main/java/com/coffeeapp/backend/config/WebConfig.java
@@ -4,16 +4,14 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@Configuration // Λέει στο Spring Boot: "Διάβασε αυτό το αρχείο κατά την εκκίνηση, έχει ρυθμίσεις!"
+@Configuration
 public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/api/**") // Ο κανόνας ισχύει για ΟΛΑ τα URL που ξεκινάνε από /api/
-                // ΠΡΟΣΟΧΗ: Για Development επιτρέπουμε τα πάντα (*).
-                // Στο ίντερνετ (Production) θα βάλουμε το ακριβές URL του Flutter App σου (π.χ. "https://my-app.com")
+        registry.addMapping("/api/**")
                 .allowedOrigins("*")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Επιτρεπόμενες κινήσεις
-                .allowedHeaders("*"); // Επιτρέπουμε όλα τα headers (π.χ. JSON, Auth tokens)
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*");
     }
 }
